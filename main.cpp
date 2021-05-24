db9fca917e35bdeac06e619ed695046519294961
jshabun <jshab003@ucr.edu>
2021-05-24 03:17:00 -0700

    main creation and setup

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+#include "add.hpp"
+#include "base.hpp"
+#include "div.hpp"
+#include "mult.hpp"
+#include "op.hpp"
+#include "pow.hpp"
+#include "rand.hpp"
+#include "sub.hpp"
+
+#include "factory_pattern.hpp"
+
+using namespace std;
+
+int main(int argc, char** agrv) {
+
+return 0;
+}
